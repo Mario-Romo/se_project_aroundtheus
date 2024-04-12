81cfb99f067be7841ff37ac68dba7d35fc3ca53d
Mario-Romo <marioromo888@gmail.com>
2024-04-12 12:55:13 -0700

    Refactor

--- scripts/index.js
+++ scripts/index.js
@@ -30,14 +30,47 @@ const initialCards = [
 
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
-const modalCloseButton = document.querySelector("#modal-close-btn");
+const profileEditCloseButton = profileEditModal.querySelector(".modal__close");
 
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector("#profile-description-input");
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+
+
+
+/*------------------------ FUNCTIONS ------------------------------*/
+
+function closePopop() {
+    profileEditModal.classList.remove("modal_opened");
+}
+
+
+
+/*------------------------ EVENT HANDLERS ------------------------------*/
+function handleProfileEditSubmit(e) {
+    e.preventDefault(); 
+    profileTitle.textContent = profileTitleInput.value;
+    profileDescription.textContent = profileDescriptionInput.value;
+    closePopop();
+}
+
+
+
+/*------------------------ EVENT LISTENERS ------------------------------*/
 
 profileEditButton.addEventListener('click', () => {
+    profileTitleInput.value = profileTitle.textContent;
+    profileDescriptionInput.value = profileDescription.textContent;
     profileEditModal.classList.add("modal_opened");
-} )
+});
 
 
-modalCloseButton.addEventListener('click', () => {
-    profileEditModal.classList.remove("modal_opened");
-} )
+
+
+profileEditCloseButton.addEventListener('click', () => {
+    closePopop();
+});
+
+profileEditForm.addEventListener('submit', handleProfileEditSubmit);
